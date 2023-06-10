cc944f20afa2915d94ada22ea65adda2407a6c0d
pkgone <praneethchandra@gmail.com>
2023-06-09 18:33:02 -0700

    Part 2 and 3: create edit people, notes

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template
-import connexion
+# import connexion
+import config
+from models import Person
 
 # app = Flask(__name__)
 
-app = connexion.App(__name__, specification_dir="./")
-app.add_api("swagger.yml")
+# app = connexion.App(__name__, specification_dir="./")
+app = config.connex_app
+# app.add_api("swagger.yml")
+app.add_api(config.basedir / "swagger.yml")
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    # return render_template("home.html")
+    people = Person.query.all()
+    return render_template("home.html", people=people)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
--- people.py
+++ people.py
@@ -1,9 +1,18 @@
-from datetime import datetime
+
 from flask import abort, make_response
+from config import db
+from models import Person, people_schema, person_schema, PersonSchema
+"""
+From Part1: 
+# from datetime import datetime
+"""
 
-def get_timestamp():
-    return datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
+""" From Part1:
+# def get_timestamp():
+#     return datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
+"""
 
+""" From Part1:
 PEOPLE= {
     "Fairy": {
         "fname": "Tooth",
@@ -21,11 +30,19 @@ PEOPLE= {
         "timestamp": get_timestamp(),
     }
 }
+"""
+
 
 def read_all():
+    """ From Part1:
     return list(PEOPLE.values())
+    """
+    people = Person.query.all()
+    person_schema = PersonSchema(many=True)
+    return person_schema.dump(people)
 
 def create(person):
+    """ From Part1:
     lname = person.get("lname")
     fname = person.get("fname", "")
 
@@ -41,16 +58,35 @@ def create(person):
             406,
             f"Person with last name {lname} already exists",
         )
+    """
+    lname = person.get("lname")
+    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+    if existing_person is None:
+        new_person = person_schema.load(person, session=db.session)
+        db.session.add(new_person)
+        db.session.commit()
+        return person_schema.dump(new_person), 201
+    else:
+        abort(406, f"Person with last name {lname} already exists")
 
 def read_one(lname):
+    """ From Part1:
     if lname in PEOPLE:
         return PEOPLE[lname]
     else:
         abort(
             404, f"Person with last name {lname} not found"
         )
+    """
+    person = Person.query.filter(Person.lname == lname).one_or_more()
+    if person is not None:
+        return person_schema.dump(person)
+    else:
+        abort(404, f"Person with last name {lname} not found")
+
 
 def update(lname, person):
+    """ From Part1:
     if lname in PEOPLE:
         PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
         PEOPLE[lname]["timestamp"] = get_timestamp()
@@ -60,8 +96,20 @@ def update(lname, person):
             404,
             f"Person with last name {lname} not found"
         )
+    """
+    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+
+    if existing_person:
+        update_person = person_schema.load(person, session=db.session)
+        existing_person.fname = update_person.fname
+        db.session.merge(existing_person)
+        db.session.commit()
+        return person_schema.dump(existing_person), 201
+    else:
+        abort(404, f"Person with last name {lname} not found")
 
 def delete(lname):
+    """ From Part1:
     if lname in PEOPLE:
         del PEOPLE[lname]
         return make_response(
@@ -72,3 +120,12 @@ def delete(lname):
             404,
             f"Person with last name {lname} not found"
         )
+    """
+    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+
+    if existing_person:
+        db.session.delete(existing_person)
+        db.session.commit()
+        return make_response(f"{lname} successfully deleted", 200)
+    else:
+        abort(404, f"Person with last name {lname} not found")
